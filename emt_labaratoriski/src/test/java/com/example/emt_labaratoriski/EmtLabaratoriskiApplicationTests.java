f1b283f3d23cc7e790a9445500490d52043f48da
Nikola Maroski <nikolamaroski@hotmail.com>
2020-04-08 00:00:16 +0200

    Emt lab initial commit

---
+++ emt_labaratoriski/src/test/java/com/example/emt_labaratoriski/EmtLabaratoriskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emt_labaratoriski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtLabaratoriskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
