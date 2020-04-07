6132f2963ffb80789524e9232406e2d58e5dd5a8
Nikola Maroski <nikolamaroski@hotmail.com>
2020-04-08 00:48:40 +0200

    Initial commit Project lab emt

---
+++ lab_163179_emt/src/main/java/com/example/lab_163179_emt/Lab163179EmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_163179_emt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab163179EmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab163179EmtApplication.class, args);
+    }
+
+}
---
+++ lab_163179_emt/src/test/java/com/example/lab_163179_emt/Lab163179EmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_163179_emt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab163179EmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
