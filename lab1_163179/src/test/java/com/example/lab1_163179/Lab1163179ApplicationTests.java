75fe8cadaa1bb82847aed0e655a7058bf0218076
Nikola Maroski <nikolamaroski@hotmail.com>
2020-04-07 23:06:07 +0200

    initial commit

---
+++ lab1_163179/src/test/java/com/example/lab1_163179/Lab1163179ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_163179;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1163179ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
